c37fb7e16bf39e1bf71833591fc3eb24c0348f3d
Mike Korcok <korcokm@yahoo.com>
2015-08-18 21:47:53 -0700

    Adding missing files.

---
+++ GeekQuiz/Controllers/TriviaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Description;
+using GeekQuiz.Models;
+
+namespace GeekQuiz.Controllers
+{
+    public class TriviaController : ApiController
+    {
+        private TriviaContext db = new TriviaContext();
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
